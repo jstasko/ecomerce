2ea3512f32f3592493b71de9000071cdee26f77f
jstasko <jozef.stasko@gjensidige.no>
2024-11-26 13:29:12 +0100

    add first app

---
+++ sb-ecomerce/src/main/java/sk/stasko/ecomerce/SbEcomerceApplication.java
@@ -0,0 +1,13 @@
+package sk.stasko.ecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcomerceApplication.class, args);
+	}
+
+}
---
+++ sb-ecomerce/src/test/java/sk/stasko/ecomerce/SbEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stasko.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
